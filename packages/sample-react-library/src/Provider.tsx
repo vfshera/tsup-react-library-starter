ade43910ac2a9f16bf4b00af8fcd0ce60f5283ab
Franklin Shera <fshera96@gmail.com>
2023-10-15 03:29:20 +0300

    fix: config provider

--- packages/sample-react-library/src/Provider.tsx
+++ packages/sample-react-library/src/Provider.tsx
@@ -1,13 +1,24 @@
-import React, { FC, ReactNode } from "react";
-import { useConfig } from "./hooks";
+import React, { FC, ReactNode, createContext, useContext } from "react";
+
+import { Config, defaultConfig } from "./config";
 
 export interface ProviderProps {
   children: ReactNode;
-  options?: { a: string; b: string }; // like config options for lib
+  options?: Partial<Config>;
 }
+const ConfigContext = createContext<Config>(defaultConfig);
 
-export const Provider: FC<ProviderProps> = ({ children, options }) => {
-  const config = useConfig();
-  console.log({ options, config });
-  return <div className="sample-library-provider">{children}</div>;
+// eslint-disable-next-line react-refresh/only-export-components
+export const useConfig = () => {
+  const config = useContext(ConfigContext);
+
+  return config;
+};
+
+export const Provider: FC<ProviderProps> = ({ children, options = {} }) => {
+  return (
+    <ConfigContext.Provider value={{ ...defaultConfig, ...options }}>
+      <div className={`sample-react-library`}>{children}</div>
+    </ConfigContext.Provider>
+  );
 };
